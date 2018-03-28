01d773f8a8477962d94e540bde9a47f65558d23f
Anıl Yılmaz <incelefon@gmail.com>
2018-03-28 16:13:25 +0300

    Login Post Method

--- SimpleBlog/Controllers/LoginController.cs
+++ SimpleBlog/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleBlog.Controllers;
+using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,12 @@ namespace SimpleBlog.Controllers
         {
             return View();
             //return Content("Login page!");
+        }
+        [HttpPost]
+        public ActionResult Index(Login form)
+        {
+            //return View();
+            return Content("Your İnformation<br>"+form.UserName+"<br>"+form.Password);
         }
     }
 }
