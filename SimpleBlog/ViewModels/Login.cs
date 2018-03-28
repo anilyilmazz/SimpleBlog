3d69e98c2c3fdaea386d92aeffc6d6a36a3343e8
Anıl Yılmaz <incelefon@gmail.com>
2018-03-28 16:36:07 +0300

    Hmtl Form to C# form in login/index.cshtml

--- SimpleBlog/ViewModels/Login.cs
+++ SimpleBlog/ViewModels/Login.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SimpleBlog.ViewModels
 {
+
+    //Login controllerdeki kullanılan login class'ın ismidir.
     public class Login
     {
         public string UserName { get; set; }
+        [DataType("Password")]
         public string Password { get; set; }
     }
 }
